b7a9892d6cb6c206d6258c49ef5f36d51e896f97
Brandon Mitchell <bcmitchell91@gmail.com>
2023-04-22 18:27:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -16,23 +16,22 @@ while True:
         mask_type = df.apply(lambda row: any(x in row[['Type1', 'Type2']].tolist() for x in choices), axis=1)
         df = df.loc[mask_type]
 
-    # Get a list of all abilities and display a dialog to select one or more abilities
+    # Get a list of all abilities and display a dialog to select one ability
     abilities = sorted(set(filter(None, df[["Ability1", "Ability2", "Ability_Hidden"]].fillna('').values.flatten().tolist())))
-    choices = easygui.multchoicebox("Select one or more abilities to filter by:", "Filter by Ability", ['Any'] + abilities)
+    choices = easygui.choicebox("Select an ability to filter by:", "Filter by Ability", ['Any'] + abilities)
     if choices is None:  # Handle close window button click
         sys.exit()
     elif 'Any' in choices:  # Handle filtering by ability
         filtered_df = df
     else:
-        # Filter the DataFrame by the selected abilities
-        mask_ability = df.apply(lambda row: any(x in row[['Ability1', 'Ability2', 'Ability_Hidden']].tolist() for x in choices), axis=1)
+        # Filter the DataFrame by the selected ability
+        mask_ability = df.apply(lambda row: choices in row[['Ability1', 'Ability2', 'Ability_Hidden']].tolist(), axis=1)
         filtered_df = df.loc[mask_ability]
 
     # Save the filtered results to a new CSV file
-    filtered_df = filtered_df[['Name', 'Type1', 'Type2', 'Ability1', 'Ability2', 'Ability_Hidden', 'HP', 'Attack', 'Defense', 'SP_Attack', 'SP_Defense', 'Speed']]
-    filtered_df.to_csv("pokemonfiltered.csv", index=False)
-
-    easygui.msgbox("Filtered results saved to pokemonfiltered.csv", "Results")
-
+    if not filtered_df.empty:
+        filtered_df = filtered_df[['Name', 'Type1', 'Type2', 'Ability1', 'Ability2', 'Ability_Hidden', 'HP', 'Attack', 'Defense', 'SP_Attack', 'SP_Defense', 'Speed']]
+        filtered_df.to_csv("pokemonfiltered.csv", index=False)
+        easygui.msgbox("Filtered results saved to pokemonfiltered.csv", "Results")
 
 
