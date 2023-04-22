9123aae8ee0fa2471b78db6340305f71b940c8e5
Brandon Mitchell <bcmitchell91@gmail.com>
2023-04-22 13:45:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
 import pandas as pd
 import easygui
 import sys
+
 # Read the CSV file into a pandas DataFrame
 df = pd.read_csv("pokemon.csv")
 
 while True:
-    # Search for pokemon of a certain type
-    type1 = easygui.enterbox("Enter a type to search for:")
-    if type1 is None:  # Handle close window button click
+    # Get a list of all types and display a dialog to select one or more types
+    types = sorted(set(df["Type1"].fillna('').tolist() + df["Type2"].fillna('').tolist()))
+    choices = easygui.multchoicebox("Select one or more types to filter by:", "Filter by Type", types)
+    if choices is None:  # Handle close window button click
         sys.exit()
-    type1_pokemon = df.loc[(df["Type1"] == type1) | (df["Type2"] == type1)]
-    easygui.textbox("Results:", str(type1_pokemon))
-
-    # Search for pokemon of a certain dual type
-    type1 = easygui.enterbox("Enter the first type to search for:")
-    if type1 is None:  # Handle close window button click
-        sys.exit()
-    type2 = easygui.enterbox("Enter the second type to search for:")
-    if type2 is None:  # Handle close window button click
-        sys.exit()
+    elif len(choices) == 0:  # Handle no type selected
         continue
-    dual_type_pokemon = df.loc[(df["Type1"] == type1) & (df["Type2"] == type2)]
-    easygui.textbox("Results:", str(dual_type_pokemon))
 
-    # Search for pokemon with a certain ability
-    ability = easygui.enterbox("Enter an ability to search for:")
-    if ability is None:  # Handle close window button click
+    # Filter the DataFrame by the selected types
+    mask = df.apply(lambda row: any(x in row[['Type1', 'Type2']].tolist() for x in choices), axis=1)
+    filtered_df = df.loc[mask]
+
+    # Get a list of all abilities and display a dialog to select one or more abilities
+    abilities = sorted(set(filtered_df[["Ability1", "Ability2", "Ability_Hidden"]].fillna('').values.flatten().tolist()))
+    choices = easygui.multchoicebox("Select one or more abilities to filter by:", "Filter by Ability", abilities)
+    if choices is None:  # Handle close window button click
         sys.exit()
-        break
-    ability_pokemon = df.loc[(df["Ability1"] == ability) | (df["Ability2"] == ability) | (df["Ability_Hidden"] == ability)]
-    easygui.textbox("Results:", str(ability_pokemon))
+    elif len(choices) == 0:  # Handle no ability selected
+        continue
+
+    # Filter the DataFrame by the selected abilities
+    mask = filtered_df.apply(lambda row: any(x in row[['Ability1', 'Ability2', 'Ability_Hidden']].tolist() for x in choices), axis=1)
+    filtered_df = filtered_df.loc[mask]
+
+    # Save the filtered results to a new CSV file
+    filtered_df.to_csv("pokemonfiltered.csv", index=False)
+
+    easygui.msgbox("Filtered results saved to pokemonfiltered.csv", "Results")
+
