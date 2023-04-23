ff81334cfe69ae4c50880214d465d4e0fb510c4b
Brandon Mitchell <bcmitchell91@gmail.com>
2023-04-23 01:08:07 -0500

    Update and rename main.py to pokemon.py
    Completed Pokemon filter project

--- pokemon.py
+++ pokemon.py
@@ -5,6 +5,11 @@ import sys
 # Read the CSV file into a pandas DataFrame
 df = pd.read_csv("pokemon.csv")
 
+# Replace NaN values in "Type2", "Ability2", and "Ability_Hidden" columns with "None"
+df["Type2"] = df["Type2"].fillna("None")
+df["Ability2"] = df["Ability2"].fillna("None")
+df["Ability_Hidden"] = df["Ability_Hidden"].fillna("None")
+
 while True:
     # Get a list of all types and display a dialog to select one or more types
     types = sorted(set(filter(None, df["Type1"].fillna('').tolist() + df["Type2"].fillna('').tolist())))
@@ -14,9 +19,8 @@ while True:
     elif 'Any' in choices and len(choices) > 1 or len(choices) > 2:  # Handle filtering by type
         easygui.msgbox("Please select no more then two types", "Error")
         continue
-        # Filter the DataFrame by the selected types
+    # Filter the DataFrame by the selected types
     elif 'Any' not in choices:
-        # Filter the DataFrame by the selected types
         mask_type = df.apply(lambda row: all(x in row[['Type1', 'Type2']].tolist() for x in choices), axis=1)
         df = df.loc[mask_type]
 
@@ -40,4 +44,3 @@ while True:
         easygui.codebox(msg="Filtered results saved to pokemonfiltered.csv", title="Results", text=result)
         df = pd.read_csv("pokemon.csv")
         filtered_df = df
-
